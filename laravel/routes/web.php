a24d8c8ed5c54aefe2a214c9ea16a694548de307
salauddin-shanto <salauddin6854@gmail.com>
2025-04-17 00:44:15 +0600

    fix(api): fixed routing of frontend from backend for successful payment

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Modules\Payment\Controllers\SslCommerzPaymentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Payment routes
+Route::prefix('payments')->group(function () {
+    Route::get('checkout/easy', [SslCommerzPaymentController::class, 'exampleEasyCheckout'])->name('payments.checkout.easy');
+    Route::get('checkout/hosted', [SslCommerzPaymentController::class, 'exampleHostedCheckout'])->name('payments.checkout.hosted');
+    
+    Route::post('initiate', [SslCommerzPaymentController::class, 'initiatePayment'])->name('payments.initiate');
+    Route::post('initiate-ajax', [SslCommerzPaymentController::class, 'initiateAjaxPayment'])->name('payments.initiate.ajax');
+    
+    Route::post('success', [SslCommerzPaymentController::class, 'handleSuccess'])->name('payments.success');
+    Route::post('fail', [SslCommerzPaymentController::class, 'handleFailure'])->name('payments.fail');
+    Route::post('cancel', [SslCommerzPaymentController::class, 'handleCancel'])->name('payments.cancel');
+    Route::post('ipn', [SslCommerzPaymentController::class, 'ipn'])->name('payments.ipn');
 });
